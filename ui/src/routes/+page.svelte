fd89cb22f6bced43560ad13f4a0b56251f6b7adc
Haru Lee <haru.lee@nmrk.com>
2024-04-18 17:00:36 -0500

    button on home, basic table

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { goto } from '$app/navigation';
+
+</script>
+
+<button on:click={() => goto('/scan')}>Scan</button>
--- ui/src/routes/products/[slug]/+page.svelte
+++ ui/src/routes/products/[slug]/+page.svelte
@@ -1,5 +1,36 @@
 <script>
-    import { page } from '$app/stores';
+	import { page } from '$app/stores';
+	export const data = [
+		{
+			vitae: 'dolorem',
+			lectus: 'ipsum',
+			quisquam: 'quia'
+		},
+		{
+			vitae: 'amet',
+			lectus: 'consectetur',
+			quisquam: 'adipisci'
+		}
+	];
 </script>
 
 Slug in URL: {$page.params.slug}
+
+<table>
+	<thead>
+		<tr>
+			{#each Object.keys(data[0]) as columnTitle}
+				<th>{columnTitle}</th>
+			{/each}
+		</tr>
+	</thead>
+	<tbody>
+		{#each Object.values(data) as row}
+			<tr>
+				{#each Object.values(row) as cell}
+					<td>{cell}</td>
+				{/each}
+			</tr>
+		{/each}
+	</tbody>
+</table>
