17956912d9c345381d5424054b3d5c14ba00beca
Haru Lee <haru.lee@nmrk.com>
2024-04-20 13:33:48 -0500

    using chemicaldetails tbl too

--- db/v0.0.2_products_chemicals_tables.sql
+++ db/v0.0.2_products_chemicals_tables.sql
@@ -58,19 +58,25 @@ INSERT INTO productTranslations (id, language, isDefault, name)
 INSERT INTO chemicals (createdAt) VALUES (NOW());
 INSERT INTO chemicalTranslations (id, language, isDefault, name)
     VALUES ((SELECT id FROM chemicals ORDER BY createdAt DESC LIMIT 1), 'en', true, 'Propylene Glycol');
+INSERT INTO chemicalDetails (id, details, source)
+    VALUES ((SELECT id FROM chemicals ORDER BY createdAt DESC LIMIT 1), 'While this chemical can be safely used in most people without irritation, and is generally recognized as safe by the FDA, it can be irritating to others', 'https://www.struthealth.com/blog/is-propylene-glycol-in-hair-products-bad-for-hair-and-scalp-health');
 INSERT INTO productsChemicals (productId, chemicalId)
     VALUES ((SELECT id FROM products ORDER BY createdAt DESC LIMIT 1), (SELECT id FROM chemicals ORDER BY createdAt DESC LIMIT 1));
 -- (run the rest of the code in a separate run)
 INSERT INTO chemicals (createdAt) VALUES (NOW());
 INSERT INTO chemicalTranslations (id, language, isDefault, name)
     VALUES ((SELECT id FROM chemicals ORDER BY createdAt DESC LIMIT 1), 'en', true, 'Cetearyl Alcohol');
+INSERT INTO chemicalDetails (id, details, source)
+    VALUES ((SELECT id FROM chemicals ORDER BY createdAt DESC LIMIT 1), 'Even though cetearyl alcohol is a safe ingredient, some people are allergic to it', 'https://www.latest-hairstyles.com/advice/is-cetearyl-alcohol-bad-for-hair.html');
 INSERT INTO productsChemicals (productId, chemicalId)
     VALUES ((SELECT id FROM products ORDER BY createdAt DESC LIMIT 1), (SELECT id FROM chemicals ORDER BY createdAt DESC LIMIT 1));
 
 -- 3. Display result
 SELECT pt.id AS productId, pt.language AS productLanguage, pt.name AS productName, pt.createdAt AS productCreatedAt, 
-    ct.id AS chemicalId, ct.language AS chemicalLanguage, ct.name AS chemicalName, ct.createdAt AS chemicalCreatedAt FROM products p
+    ct.id AS chemicalId, ct.language AS chemicalLanguage, ct.name AS chemicalName, ct.createdAt AS chemicalCreatedAt, cd.details AS chemicalDetails, cd.source AS chemicalSource
+FROM products p
 INNER JOIN productTranslations pt ON p.id = pt.id
 INNER JOIN productsChemicals pc ON p.id = pc.productId
 LEFT JOIN chemicals c ON pc.chemicalId = c.id
 INNER JOIN chemicalTranslations ct ON c.id = ct.id
+LEFT JOIN chemicalDetails cd ON c.id = cd.id
