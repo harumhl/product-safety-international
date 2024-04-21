a64cee65af069119e5d749b22faf04807e759afa
Haru Lee <haru.lee@nmrk.com>
2024-04-21 13:50:17 -0500

    more ORMs

--- backend/api/models.py
+++ backend/api/models.py
@@ -10,12 +10,30 @@ Run below commands after making changes
 
 class Product(models.Model):
     id = models.UUIDField(null=False, primary_key=True, default=uuid.uuid4().hex)
-#     barcode = models.TextField()
-#     createdAt = models.DateTimeField(auto_now_add=True)
+    barcode = models.TextField(null=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
 
-# class Chemical(models.Model):
-#     id = models.UUIDField(null=False, primary_key=True, default=uuid.uuid4().hex)
-#     createdAt = models.DateTimeField(auto_now_add=True)
+class Chemical(models.Model):
+    id = models.UUIDField(null=False, primary_key=True, default=uuid.uuid4().hex)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
 
-# class ProductChemical(models.Model):
-#     pass
+class ProductTranslation(models.Model):
+    language = models.TextField(null=False) # from https://en.wikipedia.org/wiki/IETF_language_tag
+    name = models.TextField(null=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+class ChemicalTranslation(models.Model):
+    language = models.TextField(null=False) # from https://en.wikipedia.org/wiki/IETF_language_tag
+    name = models.TextField(null=False)
+    score = models.FloatField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+    chemical = models.ForeignKey(Chemical, on_delete=models.CASCADE)
+
+class ChemicalDetails(models.Model):
+    details = models.TextField()
+    source = models.TextField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+    chemicalTranslation = models.ForeignKey(ChemicalTranslation, on_delete=models.CASCADE)
+
